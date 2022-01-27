81f7fd106d7f80725e611ed3a4fdd5cdbd1dae8e
andressamorschel <andressamorschel@gmail.com>
2022-01-27 13:50:48 -0300

    atualizando controller e service de portability

--- src/main/java/com/sysmap/srcmsportability/application/service/PortabilityServiceImpl.java
+++ src/main/java/com/sysmap/srcmsportability/application/service/PortabilityServiceImpl.java
@@ -4,6 +4,7 @@ import com.sysmap.srcmsportability.application.ports.in.PortabilityService;
 import com.sysmap.srcmsportability.application.ports.out.PortabilityRepository;
 import com.sysmap.srcmsportability.domain.entities.Portability;
 import com.sysmap.srcmsportability.domain.entities.enums.StatusPortability;
+import com.sysmap.srcmsportability.domain.entities.exceptions.PortabilityNotFound;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.ResponseEntity;
 
@@ -24,17 +25,16 @@ public class PortabilityServiceImpl implements PortabilityService {
     }
 
     @Override
-    public ResponseEntity putStatusPortability(UUID portabilityId, StatusPortability status) throws ChangeSetPersister.NotFoundException {
+    public void putStatusPortability(UUID portabilityId, StatusPortability status) {
 
         Optional<Portability> optionalPortability = portabilityRepository.findPortabilityById(portabilityId);
-        if(!optionalPortability.isPresent()) {
-            throw new ChangeSetPersister.NotFoundException();
+        if(optionalPortability.isEmpty()) {
+            throw new PortabilityNotFound("portability not found with id: " + portabilityId);
         }
+
         Portability portability = optionalPortability.get();
         portability.setStatus(status);
         portabilityRepository.savePortability(portability);
-
-        return ResponseEntity.ok(portability);
     }
 
 }
--- src/main/java/com/sysmap/srcmsportability/framework/adapters/in/PortabilityController.java
+++ src/main/java/com/sysmap/srcmsportability/framework/adapters/in/PortabilityController.java
@@ -1,32 +1,32 @@
 package com.sysmap.srcmsportability.framework.adapters.in;
 
 import com.sysmap.srcmsportability.application.ports.in.PortabilityService;
+import com.sysmap.srcmsportability.domain.entities.Portability;
 import com.sysmap.srcmsportability.framework.adapters.in.dto.InputPortability;
 import com.sysmap.srcmsportability.framework.adapters.in.dto.InputPutStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("ms-src-portability/v1")
 public class PortabilityController {
 
-    @Autowired
-    private PortabilityService portabilityService;
+    private final PortabilityService portabilityService;
 
     @PostMapping("/portability")
-    public ResponseEntity createPortability(@RequestBody InputPortability inputPortability) {
-        portabilityService.createPortability(inputPortability.getPortability());
-        return new ResponseEntity( HttpStatus.CREATED );
+    public ResponseEntity<Portability> createPortability(@RequestBody InputPortability inputPortability) {
+        return null;
     }
 
     @PutMapping("/portability/{portabilityId}")
-    public ResponseEntity putStatusPortability(@RequestBody InputPutStatus inputPutStatus, @PathVariable UUID portabilityId)
-            throws ChangeSetPersister.NotFoundException {
-        return portabilityService.putStatusPortability(portabilityId, inputPutStatus.getStatus());
+    public ResponseEntity<Void> putStatusPortability(@RequestBody InputPutStatus inputPutStatus,
+                                                     @PathVariable UUID portabilityId) {
+        portabilityService.putStatusPortability(portabilityId, inputPutStatus.getStatus());
+        return ResponseEntity.ok().build();
     }
 }
