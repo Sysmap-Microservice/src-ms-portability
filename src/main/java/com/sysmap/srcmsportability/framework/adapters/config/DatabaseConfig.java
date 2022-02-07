13134a1047b6955c28996c7fc8f9030d30e9740c
Gustavo Riposati <guriposa@gmail.com>
2022-02-07 13:22:41 -0300

    retirada ; desnecessario

--- src/main/java/com/sysmap/srcmsportability/framework/adapters/config/DatabaseConfig.java
+++ src/main/java/com/sysmap/srcmsportability/framework/adapters/config/DatabaseConfig.java
@@ -15,7 +15,7 @@ public class DatabaseConfig {
         final HikariDataSource hikariDataSource = new HikariDataSource();
 
         hikariDataSource.setDriverClassName("org.postgresql.Driver");
-        hikariDataSource.setJdbcUrl("jdbc:postgresql://ec2-54-224-64-114.compute-1.amazonaws.com:5432/ddsiu3l3tbm2b4"); ;
+        hikariDataSource.setJdbcUrl("jdbc:postgresql://ec2-54-224-64-114.compute-1.amazonaws.com:5432/ddsiu3l3tbm2b4");
         hikariDataSource.setUsername("vmwnthbejoqkeq");
         hikariDataSource.setPassword("86bfe1b3e27628a052481ebf8d89983709a22fff80e2d0f9b497e2e72e7f98ed");
 
